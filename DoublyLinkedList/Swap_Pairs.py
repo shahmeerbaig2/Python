54ebea43551e1093e3e4eaadd7d6f7e4d3331fb2
shahmeerbaig2 <shahmeerbaig2@gmaill.com>
2025-10-17 16:18:38 +0500

    new conding exercises

--- DoublyLinkedList/Swap_Pairs.py
+++ DoublyLinkedList/Swap_Pairs.py
@@ -1,13 +1,20 @@
 def swap_pairs(self):
-    if self.length<2:
-        return
-    Dummy = Node(0)
+    if self.length<=1:
+        return None
+    Dummy=Node(0)
     Dummy.next=self.head
     prev=Dummy
     first=self.head
+        
     while first and first.next:
         second=first.next
         first.next=second.next
+        second.next=first
         second.next.prev=first
+            
         prev.next=second
-        secon
+        second.prev=prev
+        prev=first
+        first=first.next
+    self.head=Dummy.next
+    return
